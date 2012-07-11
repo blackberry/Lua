1595f3e126e469f57ac106e91704f2ca4bf40c4b
Chris Culy <cculy@rim.com>
2012-07-11 16:02:27 -0400

    Updated QNX makefile (to remove source files that should not be compiled into the lib).

--- qnx/common.mk
+++ qnx/common.mk
@@ -8,6 +8,8 @@ NAME=lua
 #===== EXTRA_SRCVPATH - a space-separated list of directories to search for source files.
 EXTRA_SRCVPATH+=$(PRODUCT_ROOT)/src
 
+SRCS=lapi.c lauxlib.c lbaselib.c lbitlib.c lcode.c lcorolib.c lctype.c ldblib.c ldebug.c ldo.c ldump.c lfunc.c lgc.c linit.c liolib.c llex.c lmathlib.c lmem.c loadlib.c lobject.c lopcodes.c loslib.c lparser.c lstate.c lstring.c lstrlib.c ltable.c ltablib.c ltm.c lundump.c lvm.c lzio.c
+
 include $(MKFILES_ROOT)/qtargets.mk
 
 OPTIMIZE_TYPE_g=none
